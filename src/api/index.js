ec953c8a311018cce0cb54a9dcdf58517ef46244
Jae Chung <jaepublic@gmail.com>
2021-07-20 10:07:56 -0500

    chore: set up api calls with auth token

---
+++ src/api/index.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+export const getParamValues = (url) => {
+  return url
+    .slice(1)
+    .split('&')
+    .reduce((accumulator, currentValue) => {
+      const [title, value] = currentValue.split('=');
+      accumulator[title] = value;
+      return accumulator;
+    }, {});
+};
+
+export const setAuthHeader = () => {
+  try {
+    const params = JSON.parse(localStorage.getItem('params'));
+    if (params) {
+      axios.defaults.headers.common[
+        'Authorization'
+      ] = `Bearer ${params.access_token}`;
+    }
+  } catch (error) {
+    console.log('Error setting auth', error);
+  }
+};
+
+export const get = async (url, params) => {
+  setAuthHeader();
+  const result = await axios.get(url, params);
+  return result.data;
+};
+
+export const post = async (url, params) => {
+  setAuthHeader();
+  const result = await axios.post(url, params);
+  return result.data;
+};
