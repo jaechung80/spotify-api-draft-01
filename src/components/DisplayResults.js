18941a6a76914ec87873352f810ee7a4ff876a12
Jae Chung <jaepublic@gmail.com>
2021-07-20 15:19:37 -0500

    chore: retrieve raw api data

---
+++ src/components/DisplayResults.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DisplayResults() {
+  return <div>test</div>;
+}
+
+export default DisplayResults;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { setAuthHeader } from '../api/index';
 
 function SearchForm() {
+  const [search, setSearch] = useState('');
+
+  const handleChange = (evt) => {
+    setSearch(evt.target.value);
+  };
+
+  const handleSearch = async (evt) => {
+    evt.preventDefault();
+    console.log('in handleSearch before anything happens');
+
+    try {
+      setAuthHeader();
+      const result = await axios.get(
+        `https://api.spotify.com/v1/search?query=${encodeURIComponent(
+          search
+        )}&type=artist`
+      );
+      console.log('in handleSearch, result', result);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => console.log('here is search in state', search));
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSearch}>
+        <input
+          type='text'
+          name='search'
+          value={search}
+          onChange={handleChange}
+        />
         <button type='submit'>Search</button>
       </form>
     </div>
