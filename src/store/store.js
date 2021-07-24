bea3af4bfa15cc103c9356d2ee76f6d5d2ec4210
Jae Chung <jaepublic@gmail.com>
2021-07-23 20:51:08 -0500

    chore: add artists and tracks subreducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,16 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import albumsReducer from './albums';
+import artistsReducer from './artists';
+import tracksReducer from './tracks';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   combineReducers({
     albums: albumsReducer,
+    artists: artistsReducer,
+    tracks: tracksReducer,
   }),
   composeEnhancers(applyMiddleware(thunk))
 );
